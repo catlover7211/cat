6c397dd70990ee01e2958d40b011e2987732fba4
cc <83531778+catlover7211@users.noreply.github.com>
2024-02-14 14:41:18 +0000

    update

--- tex.js
+++ tex.js
@@ -1,101 +1,68 @@
-gsap.registerPlugin(MotionPathPlugin);
-MorphSVGPlugin.convertToPath("circle, rect");
-gsap.set("#paperPlaneRoute", { drawSVG: "0% 0%" });
-gsap.set("#rectSentItems", { x: "-=240" });
-const tl = gsap.timeline();
+const MIN_SPEED = 1.5
+const MAX_SPEED = 2.5
 
-let ranOnce = false;
-
-function onBtnUp() {
-	if (ranOnce) {
-		tl.restart();
-		return;
-	}
-	ranOnce = true;
-	tl.to("#base", { duration: 0.2, scale: 1, transformOrigin: "50% 50%" });
-	tl.to(
-		"#btnBase",
-		{ duration: 0.77, morphSVG: "#cBottom", ease: "power1.inOut" },
-		"start"
-	);
-
-	tl.to("#btnBase", { duration: 0.23, morphSVG: "#cTop", ease: "power1.inOut" });
-	tl.to("#btnBase", {
-		duration: 0.2,
-		morphSVG: "#cCenter",
-		ease: "power1.inOut"
-	});
-	tl.to(
-		"#btnBase",
-		{ duration: 0.5, morphSVG: "#cEnd", ease: "power1.inOut" },
-		"revealStart"
-	);
-	tl.to("#rectSentItems", { x: "0", duration: 0.5 }, "revealStart");
-	tl.to(
-		"#mask1",
-		{ x: "-=260", duration: 0.5, ease: "power1.inOut" },
-		"revealStart"
-	);
-	tl.to(
-		"#paperPlane",
-		{ x: "-=205", duration: 0.5, ease: "power1.inOut" },
-		"revealStart"
-	);
-	tl.to(
-		"#paperPlanePath",
-		{ duration: 0.43, morphSVG: "#tickMark" },
-		"start+=0.77"
-	);
-
-	tl.to(
-		"#txtSend",
-		{ duration: 0.6, scale: 0, transformOrigin: "50% 50%" },
-		"start"
-	);
-	tl.to(
-		"#paperPlaneRoute",
-		{ drawSVG: "80% 100%", duration: 0.7, ease: "power1.inOut" },
-		"start+=0.3"
-	);
-	tl.to(
-		"#paperPlaneRoute",
-		{ drawSVG: "100% 100%", duration: 0.2, ease: "power1.inOut" },
-		"start+=1"
-	);
-
-	tl.to(
-		"#paperPlane",
-		{
-			duration: 1,
-			ease: "power1.inOut",
-			immediateRender: true,
-			motionPath: {
-				path: "#paperPlaneRoute",
-				align: "#paperPlaneRoute",
-				alignOrigin: [0.5, 0.5],
-				autoRotate: 90
-			}
-		},
-		"start"
-	);
-
-	tl.to(
-		"#paperPlanePath",
-		{ duration: 0.15, attr: { fill: "#ffffff" } },
-		"start"
-	);
-	tl.to(
-		"#paperPlanePath",
-		{ duration: 0.15, attr: { fill: "#4E67E8" } },
-		"start+=0.77"
-	);
+function randomNumber(min, max) {
+  return Math.random() * (max - min) + min
 }
 
-function onBtnDown() {
-	gsap.timeline({ defaults: { clearProps: true } });
-	gsap.to("#base", { duration: 0.1, scale: 0.9, transformOrigin: "50% 50%" });
+class Blob {
+  constructor(el) {
+    this.el = el
+    const boundingRect = this.el.getBoundingClientRect()
+    this.size = boundingRect.width
+    this.initialX = randomNumber(0, window.innerWidth - this.size)
+    this.initialY = randomNumber(0, window.innerHeight - this.size)
+    this.el.style.top = `${this.initialY}px`
+    this.el.style.left = `${this.initialX}px`
+    this.vx =
+      randomNumber(MIN_SPEED, MAX_SPEED) * (Math.random() > 0.5 ? 1 : -1)
+    this.vy =
+      randomNumber(MIN_SPEED, MAX_SPEED) * (Math.random() > 0.5 ? 1 : -1)
+    this.x = this.initialX
+    this.y = this.initialY
+  }
+
+  update() {
+    this.x += this.vx
+    this.y += this.vy
+    if (this.x >= window.innerWidth - this.size) {
+      this.x = window.innerWidth - this.size
+      this.vx *= -1
+    }
+    if (this.y >= window.innerHeight - this.size) {
+      this.y = window.innerHeight - this.size
+      this.vy *= -1
+    }
+    if (this.x <= 0) {
+      this.x = 0
+      this.vx *= -1
+    }
+    if (this.y <= 0) {
+      this.y = 0
+      this.vy *= -1
+    }
+  }
+
+  move() {
+    this.el.style.transform = `translate(${this.x - this.initialX}px, ${
+      this.y - this.initialY
+    }px)`
+  }
 }
 
-const btn = document.getElementById("base");
-btn.addEventListener("mousedown", onBtnDown);
-btn.addEventListener("mouseup", onBtnUp);
+function initBlobs() {
+  const blobEls = document.querySelectorAll('.bouncing-blob')
+  const blobs = Array.from(blobEls).map((blobEl) => new Blob(blobEl))
+
+  function update() {
+    requestAnimationFrame(update)
+    blobs.forEach((blob) => {
+      blob.update()
+      blob.move()
+    })
+  }
+
+  requestAnimationFrame(update)
+}
+
+initBlobs()
