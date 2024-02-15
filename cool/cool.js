a5e5739d4f70c0707b5e1a581a7124f544964c3e
cc <83531778+catlover7211@users.noreply.github.com>
2024-02-15 09:00:11 +0800

    update

--- cool/cool.js
+++ cool/cool.js
@@ -1,77 +1,42 @@
-window.addEventListener("DOMContentLoaded",() => {
-	const c = new Clock21(".clock");
-});
-
-class Clock21 {
-	constructor(el) {
-		this.el = document.querySelector(el);
-
-		this.init();
-	}
-	init() {
-		this.timeUpdate();
-	}
-	get timeAsObject() {
-		const date = new Date();
-		const h = date.getHours();
-		const m = date.getMinutes();
-		const s = date.getSeconds();
-
-		return { h, m, s };
-	}
-	get timeAsString() {
-		const [h,m,s,ap] = this.timeDigitsGrouped;
-
-		return `${h}:${m}:${s} ${ap}`;
-	}
-	get timeDigitsGrouped() {
-		// this accessible string uses the 12-hour clock
-		let { h, m, s } = this.timeAsObject;
-		const ap = h > 11 ? "PM" : "AM";
-		// deal with midnight
-		if (h === 0) h += 12;
-		else if (h > 12) h -= 12;
-		// prepend 0 to the minute and second if single digits
-		if (m < 10) m = `0${m}`;
-		if (s < 10) s = `0${s}`;
-
-		return [h,m,s,ap];
-	}
-	animateHand(hand) {
-		const time = this.timeAsObject;
-		const minFraction = time.s / 60;
-		const angleB = Utils.decPlaces(360 * minFraction,3);
-		const angleA = angleB - 6;
-
-		this.el?.querySelector(`.clock__hand--${hand}`)?.animate(
-			[
-				{ transform: `rotate(${angleA}deg)` },
-				{ transform: `rotate(${angleB}deg)` },
-			],
-			{ duration: 300, easing: "cubic-bezier(0.65, 0, 0.35, 1)" }
+document.addEventListener("DOMContentLoaded", function () {
+	const heroGrid = document.querySelector(".tiltgrid");
+	const hero = document.querySelector(".hero");
+	const tiltDegree = 7;
+	function isElementInViewport(element) {
+		const rect = element.getBoundingClientRect();
+		return (
+			rect.bottom > 0 &&
+			rect.right > 0 &&
+			rect.left < (window.innerWidth || document.documentElement.clientWidth) &&
+			rect.top < (window.innerHeight || document.documentElement.clientHeight)
 		);
 	}
-	timeUpdate() {
-		// update the accessible timestamp in the `title`
-		this.el?.setAttribute("title", this.timeAsString);
-		// move the hands
-		const time = this.timeAsObject;
-		const minFraction = time.s / 60;
-		const hrFraction = (time.m + minFraction) / 60;
-		const twelveHrFraction = (time.h + hrFraction) / 12;
-
-		this.el?.style.setProperty("--secAngle",`${Utils.decPlaces(360 * minFraction,3)}deg`);
-		this.el?.style.setProperty("--minAngle",`${Utils.decPlaces(360 * hrFraction,3)}deg`);
-		this.el?.style.setProperty("--hrAngle",`${Utils.decPlaces(360 * twelveHrFraction,3)}deg`);
-
-		this.animateHand("s");
-		// loop
-		clearTimeout(this.timeUpdateLoop);
-		this.timeUpdateLoop = setTimeout(this.timeUpdate.bind(this),1e3);
+	function mapRange(value, inMin, inMax, outMin, outMax) {
+		return ((value - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
 	}
-}
-class Utils {
-	static decPlaces(n,d) {
-		return Math.round(n * 10 ** d) / 10 ** d;
+	function updateScrollPos() {
+		const scrollPos = window.scrollY;
+		const rect = heroGrid.getBoundingClientRect();
+		const scrollPercent = (scrollPos * 100) / rect.height;
+
+		let mappedValue = mapRange(scrollPercent, 0, 100, tiltDegree * -1, tiltDegree);
+		heroGrid.style.setProperty("--scroll-tilt", `${mappedValue}`);
+		let heroOpacity = mapRange(scrollPercent, 0, 10, 1, 0);
+		let heroScale = mapRange(scrollPercent, 0, 10, 1, 0);
+		let heroTransformY = mapRange(scrollPercent, 0, 10, 0, -100);
+		let heroBlur = mapRange(scrollPercent, 0, 10, 0, 20);
+		hero.style.opacity = heroOpacity;
+		hero.style.filter = `blur(${heroBlur}px)`;
+		hero.style.transform = `translateY(${heroTransformY}px)`;
 	}
-}
+
+	function handleScroll() {
+		if (heroGrid && isElementInViewport(heroGrid)) {
+			updateScrollPos();
+		}
+	}
+
+	window.addEventListener("scroll", handleScroll);
+
+	handleScroll();
+});
