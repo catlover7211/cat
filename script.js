087035c16ad4ce1027fec2832565269a13a29223
cc <83531778+catlover7211@users.noreply.github.com>
2024-02-15 08:26:59 +0800

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 gsap.registerPlugin(ScrollTrigger);
-let speed1 = 100;
+let speed = 100;
 
 /*  SCENE 1 */
 let scene1 = gsap.timeline();
@@ -12,18 +12,18 @@ ScrollTrigger.create({
 });
 
 // hills animation 
-scene1.to("#h1-1", { y: 3 * speed1, x: 1 * speed1, scale: 0.9, ease: "power1.in" }, 0)
-scene1.to("#h1-2", { y: 2.6 * speed1, x: -0.6 * speed1, ease: "power1.in" }, 0)
-scene1.to("#h1-3", { y: 1.7 * speed1, x: 1.2 * speed1 }, 0.03)
-scene1.to("#h1-4", { y: 3 * speed1, x: 1 * speed1 }, 0.03)
-scene1.to("#h1-5", { y: 2 * speed1, x: 1 * speed1 }, 0.03)
-scene1.to("#h1-6", { y: 2.3 * speed1, x: -2.5 * speed1 }, 0)
-scene1.to("#h1-7", { y: 5 * speed1, x: 1.6 * speed1 }, 0)
-scene1.to("#h1-8", { y: 3.5 * speed1, x: 0.2 * speed1 }, 0)
-scene1.to("#h1-9", { y: 3.5 * speed1, x: -0.2 * speed1 }, 0)
+scene1.to("#h1-1", { y: 3 * speed, x: 1 * speed, scale: 0.9, ease: "power1.in" }, 0)
+scene1.to("#h1-2", { y: 2.6 * speed, x: -0.6 * speed, ease: "power1.in" }, 0)
+scene1.to("#h1-3", { y: 1.7 * speed, x: 1.2 * speed }, 0.03)
+scene1.to("#h1-4", { y: 3 * speed, x: 1 * speed }, 0.03)
+scene1.to("#h1-5", { y: 2 * speed, x: 1 * speed }, 0.03)
+scene1.to("#h1-6", { y: 2.3 * speed, x: -2.5 * speed }, 0)
+scene1.to("#h1-7", { y: 5 * speed, x: 1.6 * speed }, 0)
+scene1.to("#h1-8", { y: 3.5 * speed, x: 0.2 * speed }, 0)
+scene1.to("#h1-9", { y: 3.5 * speed, x: -0.2 * speed }, 0)
 
 //animate text
-scene1.to("#info", { y: 8 * speed1 }, 0)
+scene1.to("#info", { y: 8 * speed }, 0)
 
 
 
